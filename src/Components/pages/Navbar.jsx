17535cf2c8fdbc786de5e3802841f29e316436bd
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-08-09 17:10:09 -0300

    Hice un condicional en el elemento de la lista Carrito para que cambie de color al agregar un producto

--- src/Components/pages/Navbar.jsx
+++ src/Components/pages/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
+import useCarritoStore from "../../Stores/carritoStore"
 
 const Navbar = () => {
+
+  const totalItems = useCarritoStore((state) => state.getTotalItems());
   return (
     <header>
       <div className="container">
@@ -30,7 +33,7 @@ const Navbar = () => {
                   <Link className="nav-link nav-a active">Registrarse</Link>
                 </li>
                 <li className="nav-item ms-2">
-                  <Link className="nav-link nav-a active" to="/Carrito">Carrito</Link>
+                  <Link className={`nav-link nav-a active ${totalItems > 0 ? 'blue-link' : ''}`} to="/Carrito">Carrito</Link>
                 </li>
               </ul>
             </div>
@@ -42,39 +45,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-/*const Navbar = () => {
-  return (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <Link to="/">
-      <h3 className="navbar-brand">HOME</h3>
-    </Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <h3 className="nav-link" >Features</h3>
-        </li>
-        <Link to="/carrito">
-          <li className="nav-item active">
-            <h3 className="nav-link" >CARRITO</h3>
-          </li>
-        </Link>
-        <li className="nav-item dropdown">
-          <h3 className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown link
-          </h3>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <h3 className="dropdown-item" >Action</h3>
-            <h3 className="dropdown-item" >Another action</h3>
-            <h3 className="dropdown-item" >Something else here</h3>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  )
-}
-export default Navbar*/
