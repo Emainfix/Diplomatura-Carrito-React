f8e4ba283e653fd4feb148072163d4164c64b128
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-08-09 19:14:29 -0300

    Nueva ruta al formulario

--- src/routing/Rutas.jsx
+++ src/routing/Rutas.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Components/pages/Home";
 import Cart from "../Components/pages/Cart/Cart";
+import RegisterForm from "../Components/pages/RegisterForm";
 
 const Rutas = () => {
   return (
     <BrowserRouter>
           <Routes>
               <Route path= "/" element={<Home/>} />
+              <Route path="/registro" element={<RegisterForm/>}/>
               <Route path="/carrito" element={<Cart/>}/>
           </Routes>
     </BrowserRouter>
