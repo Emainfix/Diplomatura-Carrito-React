a1e9a6d906cb8303be6f84432fc4b1e0e1f297d9
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-08-08 21:56:14 -0300

    Ajuste el estilo

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -5,13 +5,13 @@ import Navbar from "./Navbar"
 
 const Home = () =>{
     return (
-    <>
+    <div>
       <Navbar/>
       <div className="container">
         <div className="row d-flex justify-content-center"><Products/></div>
       </div>
       
-    </>
+    </div>
   )
 }
 
