bd7884294873e7f7974cc9f936735939cb64871f
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-08-08 21:57:02 -0300

    Cambie el diseño por el de mi anterior proyecto ajustando etiquetas a las de jsx

--- src/Components/pages/Navbar.jsx
+++ src/Components/pages/Navbar.jsx
@@ -2,6 +2,49 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
+    <header>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              <img className="ms-auto d-block" src='https://yavicrlti1ts8lkf.public.blob.vercel-storage.com/Logo_Blanco_color.svg' height="24" alt="airvex logotipo" />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item mx-2">
+                  <Link className="nav-link nav-a active" to="/">Home</Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link nav-a active">Registrarse</Link>
+                </li>
+                <li className="nav-item ms-2">
+                  <Link className="nav-link nav-a active" >Carrito</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
+
+/*const Navbar = () => {
+  return (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <Link to="/">
       <h3 className="navbar-brand">HOME</h3>
@@ -34,4 +77,4 @@ const Navbar = () => {
   </nav>
   )
 }
-export default Navbar
+export default Navbar*/
