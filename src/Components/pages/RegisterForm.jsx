8c192e07fe287a4caa86c6a05582fb4467e3d34b
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-08-09 23:18:44 -0300

    Ajusté el estilo y problemas de ids entre otros

--- src/Components/pages/RegisterForm.jsx
+++ src/Components/pages/RegisterForm.jsx
@@ -18,10 +18,9 @@ const RegisterForm = () => {
             <div className="col-12 col-lg-6 container-form">
                 <form onSubmit={enviado} className="contacto" action="">
                     
-                    {/* Campo Nombre */}
                     <div className="mb-3">
                         <label htmlFor="nombre" className="form-label">Nombre</label>
-                        <input className="campo form-control" type="text" {...register("nombre", {
+                        <input id="nombre" className="campo form-control" type="text" {...register("nombre", {
                             required: { value: true, message: "Debes colocar tu nombre" },
                             minLength: { value: 2, message: "El nombre debe tener un mínimo de 2 caracteres" },
                             maxLength: { value: 40, message: "El nombre debe tener un máximo de 40 caracteres" }
@@ -29,20 +28,18 @@ const RegisterForm = () => {
                         {errors.nombre && <span className="text-danger">{errors.nombre.message}</span>}
                     </div>
 
-                    {/* Campo Correo */}
                     <div className="mb-3">
                         <label htmlFor="correo" className="form-label">Correo</label>
-                        <input className="form-control" type="email" {...register("correo", {
+                        <input id="correo" className="form-control" type="email" {...register("correo", {
                             required: { value: true, message: "Debes colocar tu dirección de correo" },
                             pattern: { value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/, message: "La dirección de correo no es válida" }
                         })} />
                         {errors.correo && <span className="text-danger">{errors.correo.message}</span>}
                     </div>
 
-                    {/* Campo Contraseña */}
                     <div className="mb-3">
                         <label htmlFor="contraseña" className="form-label">Contraseña</label>
-                        <input className="form-control" type="password" {...register("contraseña", {
+                        <input id="contraseña" className="form-control" type="password" {...register("contraseña", {
                             required: { value: true, message: "Introduzca una contraseña" },
                             minLength: { value: 4, message: "La contraseña debe tener un mínimo de 4 caracteres" },
                             maxLength: { value: 20, message: "La contraseña debe tener un máximo de 20 caracteres" }
@@ -50,20 +47,18 @@ const RegisterForm = () => {
                         {errors.contraseña && <span className="text-danger">{errors.contraseña.message}</span>}
                     </div>
 
-                    {/* Campo Confirmar Contraseña */}
                     <div className="mb-3">
                         <label htmlFor="confirmarContraseña" className="form-label">Confirmar Contraseña</label>
-                        <input className="form-control" type="password" {...register("confirmarContraseña", {
+                        <input id="confirmarContraseña" className="form-control" type="password" {...register("confirmarContraseña", {
                             required: { value: true, message: "Requerido" },
                             validate: value => value === watch("contraseña") || "Las contraseñas deben coincidir"
                         })} />
                         {errors.confirmarContraseña && <span className="text-danger">{errors.confirmarContraseña.message}</span>}
                     </div>
 
-                    {/* Campo Fecha de Nacimiento */}
                     <div className="mb-3">
                         <label htmlFor="fechaNacimiento" className="form-label">Fecha de Nacimiento</label>
-                        <input className="form-control" type="date" {...register("fechaNacimiento", {
+                        <input id="fechaNacimiento" className="form-control" type="date" {...register("fechaNacimiento", {
                             required: { value: true, message: "Ingrese su fecha de nacimiento" },
                             validate: (value) => {
                                 const fechaNacimiento = new Date(value);
@@ -75,34 +70,31 @@ const RegisterForm = () => {
                         {errors.fechaNacimiento && <span className="text-danger">{errors.fechaNacimiento.message}</span>}
                     </div>
                     
-                    {/* Campo País */}
                     <div className="mb-3">
                         <label htmlFor="pais" className="form-label">País</label>
-                        <select className="form-select" {...register("pais")} name="" id="">
+                        <select id="pais" className="form-select" {...register("pais")}>
                             <option value="ar">Argentina</option>
                             <option value="ch">Chile</option>
                             <option value="mx">México</option>
                         </select>
                     </div>
                 
-                    {/* Campo Términos y Condiciones */}
-<div className="mb-3">
-    <div className="form-check">
-        <input 
-            className="form-check-input" 
-            type="checkbox" 
-            id="terminos" // El id es crucial para asociarlo al label
-            {...register("terminos", {
-                required: { value: true, message: "Debes aceptar los términos y condiciones" }
-            })} 
-        />
-        <label className="form-check-label" htmlFor="terminos">
-            Acepto los términos y condiciones
-        </label>
-    </div>
-    {/* El mensaje de error debe ir fuera del div.form-check */}
-    {errors.terminos && <span className="d-block text-danger">{errors.terminos.message}</span>}
-</div>
+                    <div className="mb-3">
+                        <div className="d-flex justify-content-start align-items-center">
+                            <input 
+                                className="caja form-check-input my-2 me-2" 
+                                type="checkbox" 
+                                id="terminos"
+                                {...register("terminos", {
+                                    required: { value: true, message: "Debes aceptar los términos y condiciones" }
+                                })} 
+                            />
+                            <label className="form-check-label my-2" htmlFor="terminos">
+                                Acepto los términos y condiciones
+                            </label>
+                        </div>
+                        {errors.terminos && <span className="d-block text-danger">{errors.terminos.message}</span>}
+                    </div>
                     
                     <button className="btn-enviar btn btn-primary w-100" type="submit">
                         Enviar
